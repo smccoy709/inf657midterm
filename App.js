f2a17f64268d5ed038c7095343974f52237c795d
smccoy709 <108828888+smccoy709@users.noreply.github.com>
2024-03-07 17:06:15 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import ShoppingList from './components/ShoppingList';
+import store from './components/ItemList';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+       <ShoppingList />
+    </Provider>
+  
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
