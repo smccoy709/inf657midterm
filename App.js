740dc2fde4a12d452f08a94c99343fa45836e9ba
smccoy709 <108828888+smccoy709@users.noreply.github.com>
2024-03-10 09:53:01 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import ShoppingList from './components/ShoppingList';
